3c5c846afb708d1005139fd78a5cd72cb377dd64
josephmfaulkner <josephmfaulkner@gmail.com>
2021-07-17 14:22:45 -0700

    TS Build Errors

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,10 +4,9 @@ import MessageService from '../services/MessageService';
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-    const staticMessage : string = await MessageService.getMessage();
     const dbMessages : any = await MessageService.getAllMessages();
 
-    res.json({ staticMessage: staticMessage, dbMessages: dbMessages });
+    res.json({ messages: dbMessages });
 
 });
 
